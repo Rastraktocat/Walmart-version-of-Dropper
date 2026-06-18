a42724f88d72db066f5c26f0e937fec91b4918fd
Adi <adindeshpande@gmail.com>
2026-06-18 11:16:01 -0500

    Added broken logging and script gen

--- Linux_mint_22.3/script_runners.sh
+++ Linux_mint_22.3/script_runners.sh
@@ -1,36 +1,58 @@
 build_types=(release debug)
 architecture=(86 64)
 encoding=(both xor base64)
-arr=( $(shuf -i 0-255 -n 10) )
-both_arr=( $(shuf -i 0-255 -n 10) )
+arr=( $(shuf -i 0-255 -n 3) )
+both_arr=( $(shuf -i 0-255 -n 3) )
 out="all_exe_combinations/exe_num"
 i=0
+print_log=""
 
 for build in "${build_types[@]}"; do
 	for arch in "${architecture[@]}"; do
 		for enc in "${encoding[@]}"; do
 			i=$((i + 1))
+			print_log+="\nRun number $i:\nBuild type: "
+			print_log+="$build\n"
+			print_log+="Architecture: "
+			print_log+="$arch\n"
+			print_log+="Encoding: "
+			print_log+="$enc\n\n"
+
 			if [[ "$enc" == "both" ]]; then
-				for a_idx in "${both_arr[@]}"; do
-					python3 linux_mint_script.py --hardcode --no-decode --no-encode \
-					 "--$build" --architecture "$arch" --base64 \
-					--xor-key "$a_idx" \
-					--output "${out}${i}"
+				for a_idx in "${arr[@]}"; do
+					print_log+="Encoding values: $a_idx \n"
+#					python3 linux_mint_script.py --hardcode --no-decode --no-encode \
+#					 "--$build" --architecture "$arch" \
+#					--base64 --xor-key "$a_idx" \
+#					--output "${out}${i}"
+					i=$((i + 1))
+					print_log+="Run number: $i "
 				done
 			fi
 
 			if [[ "$enc" == "xor" ]]; then
 				for a_idx in "${both_arr[@]}"; do
-					python3 linux_mint_script.py --hardcode --no-decode --no-encode \
-					"--$build" --architecture "$arch" \
-					--base64 --xor-key "$a_idx" \
-					--output "${out}${i}"
+					print_log+="Encoding values: $a_idx \n"
+#					python3 linux_mint_script.py --hardcode --no-decode --no-encode \
+#					"--$build" --architecture "$arch" \
+#				        --xor-key "$a_idx" \
+#					--output "${out}${i}"
+					i=$((i + 1))
+					print_log+="Run number $i "
 				done
 			fi
 
 			if [[ "$enc" == "base64" ]]; then
-				python3 linux_mint_script.py --hardcode --no-decode --no-encode "--$build" --architecture "$arch" --base64 --xor-key "$a_idx" --output "$out$i"
+				print_log+=" Encoding values: base64"
+#				python3 linux_mint_script.py --hardcode --no-decode --no-encode \
+#				"--$build" --architecture "$arch" \
+#				--base64 \
+#				--output "${out}${i}"
 			fi
+
+			print_log+="\n"
 		done
 	done
 done
+
+printf "$print_log" > log_file.txt
