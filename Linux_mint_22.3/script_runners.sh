9115a12e711f29ad2d3fa4a65702ceb94263e0f5
Adi <adindeshpande@gmail.com>
2026-07-13 12:25:04 -0500

    compied with the correct encode flag in script runners

--- Linux_mint_22.3/script_runners.sh
+++ Linux_mint_22.3/script_runners.sh
@@ -13,7 +13,7 @@ input="FileSystem_exe_rebuild/FileSystem_exe_rebuild.cpp"
 out="all_exe_combinations/exe_num"
 resource="FileSystem_exe_rebuild/Resource.rc"
 header="FileSystem_exe_rebuild/resource.h"
-encode="FileSystem_exe_rebuild/calc.exe"
+encode="FileSystem_exe_rebuild/calc1.exe"
 i=0
 print_log=""
 
@@ -149,24 +149,24 @@ for build in "${build_types[@]}"; do
 	done
 done
 
-python3 linux_mint_script.py --debug --hardcode --xor-key 24 --base64 --temp "\\\exe_num14.exe" --output "all_exe_combinations/exe_num14.exe"
-python3 linux_mint_script.py --debug --hardcode --xor-key 179 --base64 --temp "\\\exe_num15.exe" --output "all_exe_combinations/exe_num15.exe"
-python3 linux_mint_script.py --debug --hardcode --xor-key 67 --base64 --temp "\\\exe_num16.exe" --output "all_exe_combinations/exe_num16.exe"
+python3 linux_mint_script.py --debug --hardcode --xor-key 24 --base64 --encode "$encode" --temp "\\\exe_num14.exe" --output "all_exe_combinations/exe_num14.exe"
+python3 linux_mint_script.py --debug --hardcode --xor-key 179 --base64 --encode "$encode" --temp "\\\exe_num15.exe" --output "all_exe_combinations/exe_num15.exe"
+python3 linux_mint_script.py --debug --hardcode --xor-key 67 --base64 --encode "$encode" --temp "\\\exe_num16.exe" --output "all_exe_combinations/exe_num16.exe"
 
-python3 linux_mint_script.py --debug --hardcode --xor-key 171 --temp "\\\exe_num17.exe" --output "all_exe_combinations/exe_num17.exe"
-python3 linux_mint_script.py --debug --hardcode --xor-key 52 --temp "\\\exe_num18.exe" --output "all_exe_combinations/exe_num18.exe"
-python3 linux_mint_script.py --debug --hardcode --xor-key 128 --temp "\\\exe_num19.exe" --output "all_exe_combinations/exe_num19.exe"
+python3 linux_mint_script.py --debug --hardcode --xor-key 171 --encode "$encode" --temp "\\\exe_num17.exe" --output "all_exe_combinations/exe_num17.exe"
+python3 linux_mint_script.py --debug --hardcode --xor-key 52 --encode "$encode" --temp "\\\exe_num18.exe" --output "all_exe_combinations/exe_num18.exe"
+python3 linux_mint_script.py --debug --hardcode --xor-key 128 --encode "$encode" --temp "\\\exe_num19.exe" --output "all_exe_combinations/exe_num19.exe"
 
-python3 linux_mint_script.py --debug --hardcode --base64 --temp "\\\exe_num20.exe" --output "all_exe_combinations/exe_num20.exe"
+python3 linux_mint_script.py --debug --hardcode --base64 --encode "$encode" --temp "\\\exe_num20.exe" --output "all_exe_combinations/exe_num20.exe"
 
-python3 linux_mint_script.py --architecture 86 --debug --hardcode --xor-key 21 --base64 --temp "\\\exe_num21.exe" --output "all_exe_combinations/exe_num21.exe"
-python3 linux_mint_script.py --architecture 86 --debug --hardcode --xor-key 37 --base64 --temp "\\\exe_num22.exe" --output "all_exe_combinations/exe_num22.exe"
-python3 linux_mint_script.py --architecture 86 --debug --hardcode --xor-key 253 --base64 --temp "\\\exe_num23.exe" --output "all_exe_combinations/exe_num23.exe"
+python3 linux_mint_script.py --architecture 86 --debug --hardcode --xor-key 21 --base64 --encode "$encode" --temp "\\\exe_num21.exe" --output "all_exe_combinations/exe_num21.exe"
+python3 linux_mint_script.py --architecture 86 --debug --hardcode --xor-key 37 --base64 --encode "$encode" --temp "\\\exe_num22.exe" --output "all_exe_combinations/exe_num22.exe"
+python3 linux_mint_script.py --architecture 86 --debug --hardcode --xor-key 253 --base64 --encode "$encode" --temp "\\\exe_num23.exe" --output "all_exe_combinations/exe_num23.exe"
 
-python3 linux_mint_script.py --architecture 86 --debug --hardcode --xor-key 223 --temp "\\\exe_num24.exe" --output "all_exe_combinations/exe_num24.exe"
-python3 linux_mint_script.py --architecture 86 --debug --hardcode --xor-key 30 --temp "\\\exe_num25.exe" --output "all_exe_combinations/exe_num25.exe"
-python3 linux_mint_script.py --architecture 86 --debug --hardcode --xor-key 149 --temp "\\\exe_num26.exe" --output "all_exe_combinations/exe_num26.exe"
+python3 linux_mint_script.py --architecture 86 --debug --hardcode --xor-key 223 --encode "$encode" --temp "\\\exe_num24.exe" --output "all_exe_combinations/exe_num24.exe"
+python3 linux_mint_script.py --architecture 86 --debug --hardcode --xor-key 30 --encode "$encode" --temp "\\\exe_num25.exe" --output "all_exe_combinations/exe_num25.exe"
+python3 linux_mint_script.py --architecture 86 --debug --hardcode --xor-key 149 --encode "$encode" --temp "\\\exe_num26.exe" --output "all_exe_combinations/exe_num26.exe"
 
-python3 linux_mint_script.py --architecture 86 --debug --hardcode --base64 --temp "\\\exe_num27.exe" --output "all_exe_combinations/exe_num27.exe"
+python3 linux_mint_script.py --architecture 86 --debug --hardcode --base64 --encode "$encode" --temp "\\\exe_num27.exe" --output "all_exe_combinations/exe_num27.exe"
 
 printf "$print_log">log_file.txt
