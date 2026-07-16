dd6515efd1e968e1e4abd47645fbeee29d2d8be6
Rastraktocat <adindeshpande@gmail.com>
2026-07-16 11:05:35 -0500

    Fix process creation in test.cpp
    Updated process creation code to use CreateProcessA with proper initialization.

--- Linux_mint_22.3/FileSystem_exe_rebuild/test.cpp
+++ Linux_mint_22.3/FileSystem_exe_rebuild/test.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
 int main(){
-	name = "C:\Windows\System32\calc.exe";
-	CreateProcess(name, 
+	name = "C:\\Windows\\System32\\calc.exe";
+
+	STARTUPINFOA si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+	BOOL err = CreateProcessA(name, NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+	if (!err) {
+		printf("%u\n", GetLastError());
+	}
 	return 0;
 }
