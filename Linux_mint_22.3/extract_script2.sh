29859b4d4a3f172693da164a2a48c876a805578d
Adi <adindeshpande@gmail.com>
2026-07-25 12:42:32 -0500

    added extract so I can test on cuckoo sandbox

--- Linux_mint_22.3/extract_script2.sh
+++ Linux_mint_22.3/extract_script2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python3 linux_mint_script.py --hardcode --output "all_exe_combinations/exe_num29.exe" --temp "\\\\exe_num29.exe" --both-encoding \
---resource "FileSystem_exe_rebuild/W11_resource.rc" --multiple-files --encode-list "FileSystem_exe_rebuild/payload/payload.bat" \
+--resource "FileSystem_exe_rebuild/W11_resource.rc" --extract --multiple-files --encode-list "FileSystem_exe_rebuild/payload/payload.bat" \
 "FileSystem_exe_rebuild/payload/w11_calc.exe"
