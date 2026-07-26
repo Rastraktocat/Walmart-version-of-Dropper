4d5f236c56c2a8b7876a8826b5c42562de743769
Adi Deshpande <adindeshpande@gmail.com>
2026-07-25 19:08:02 -0700

    so I can merge

--- Linux_mint_22.3/FileSystem_exe_rebuild/payload/payload.bat
+++ Linux_mint_22.3/FileSystem_exe_rebuild/payload/payload.bat
@@ -2,6 +2,6 @@
 
 cd %USERPROFILE%\Downloads
 
-cp "C:\Windows\System32\calc.exe" "%USERPROFILE%\Downloads"
+copy "C:\Windows\System32\calc.exe" "%USERPROFILE%\Downloads"
 
 ren "calc.exe" "W11_calc.exe"
