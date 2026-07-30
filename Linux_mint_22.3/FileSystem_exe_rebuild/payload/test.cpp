3c62c6d14010880c9180cc884651e4fa0f44233d
Adi <adindeshpande@gmail.com>
2026-07-30 18:31:56 -0500

    added a file to test the network proxy

---
+++ Linux_mint_22.3/FileSystem_exe_rebuild/payload/test.cpp
@@ -0,0 +1,20 @@
+#include<windows.h>
+#include<string>
+
+void run(){
+
+	STARTUPINFOW si{};
+	PROCESS_INFORMATION pi{};
+	si.cb = sizeof(si);
+
+	std::wstring cmd = L"cmd.exe netsh winhttp show proxy";
+	CreateProcessW(nullptr, (LPWSTR) cmd.c_str(), nullptr, nullptr, FALSE, 0, nullptr, nullptr, &si, &pi);
+
+}
+
+int main(){
+
+	run();
+	return 0;
+
+}
