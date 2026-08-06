c8d73a2e8e5503ac0b7844f17d60419afe885288
Adi <adindeshpande@gmail.com>
2026-08-06 10:14:50 -0500

    compiled for an ipv6 returning endpoint

--- Linux_mint_22.3/FileSystem_exe_rebuild/payload/get_ip.cpp
+++ Linux_mint_22.3/FileSystem_exe_rebuild/payload/get_ip.cpp
@@ -166,7 +166,7 @@ std::string get_public_ip(){
 
 #endif
 
-    HINTERNET h_connect = WinHttpConnect( h_session, L"api.ipify.org", INTERNET_DEFAULT_HTTPS_PORT, 0);
+    HINTERNET h_connect = WinHttpConnect( h_session, L"ifconfig.me/ip", INTERNET_DEFAULT_HTTPS_PORT, 0);
 
     if (!h_connect) {
         DWORD err = GetLastError();
