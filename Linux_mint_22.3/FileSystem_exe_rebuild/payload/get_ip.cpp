117191bf1b8e54c8798987635193c0756f48ef7c
Adi <adindeshpande@gmail.com>
2026-08-06 10:36:46 -0500

    test if api.ipify.org has a recieve issue the same as the icanhazip.com

--- Linux_mint_22.3/FileSystem_exe_rebuild/payload/get_ip.cpp
+++ Linux_mint_22.3/FileSystem_exe_rebuild/payload/get_ip.cpp
@@ -166,7 +166,7 @@ std::string get_public_ip(){
 
 #endif
 
-    HINTERNET h_connect = WinHttpConnect( h_session, L"icanhazip.com", INTERNET_DEFAULT_HTTPS_PORT, 0);
+    HINTERNET h_connect = WinHttpConnect( h_session, L"api.ipify.org", INTERNET_DEFAULT_HTTPS_PORT, 0);
 
     if (!h_connect) {
         DWORD err = GetLastError();
