644ef9c0f2abb689e6fe2de8353e3ac1cc496d8f
Adi <adindeshpande@gmail.com>
2026-08-12 13:55:59 -0500

    added a print check

--- Linux_mint_22.3/FileSystem_exe_rebuild/FileSystem_exe_rebuild.cpp
+++ Linux_mint_22.3/FileSystem_exe_rebuild/FileSystem_exe_rebuild.cpp
@@ -236,9 +236,9 @@ int main(int argc, char* argv[])
 		drop(size5, data5, name5);
 //		drop(size1, data1, name1);
 
-		wprintf("[%s]", name4.c_str());
-		wprintf("[%s]", name5.c_str());
-		
+		wprintf(L"[%s]", name4.c_str());
+		wprintf(L"[%s]", name5.c_str());
+
 //		exe_launch(name1);
 		exe_launch(name4);
 
