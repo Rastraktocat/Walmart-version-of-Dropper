9c02c0af5f9e907166d0a8a541b55db18db7826d
Rastraktocat <adindeshpande@gmail.com>
2026-09-02 21:02:12 -0500

    Refactor network_payload.cpp for socket communication
    Updated network payload to include socket connection and message sending.

--- Linux_mint_22.3/FileSystem_exe_rebuild/payloads/network_payload.cpp
+++ Linux_mint_22.3/FileSystem_exe_rebuild/payloads/network_payload.cpp
@@ -1,8 +1,30 @@
-#include<cstdlib.h>
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
+int main() {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
 
-int main(){
+    sockaddr_in server{};
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
 
-	return 0;
+    if (connect(sock, (sockaddr*)&server, sizeof(server)) < 0) {
+        perror("connect");
+        close(sock);
+        return 1;
+    }
 
+    const char* message = "Hello World";
+    send(sock, message, strlen(message), 0);
+
+    close(sock);
+    return 0;
 }
