a8d739869184903793299b3120385c3aa0791812
Adi <adindeshpande@gmail.com>
2026-09-05 19:54:32 -0500

    updated the payload to be for windows

--- Linux_mint_22.3/FileSystem_exe_rebuild/payloads/network_payload.cpp
+++ Linux_mint_22.3/FileSystem_exe_rebuild/payloads/network_payload.cpp
@@ -1,31 +1,55 @@
 #include <iostream>
 #include <cstring>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+std::string message = "This is a message sent from the dropper";
+
+int result;
+
+int send_message(std::string client_message) {
+
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		std::cerr << "WSAStartup failed.\n";
+		return 1;
+	}
+
+	SOCKET client_connect = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (client_connect == INVALID_SOCKET){
+		std::cerr << "Socket failed.\n";
+		return 1;
+	}
+
+	sockaddr_in client_addr{};
+	client_addr.sin_family = AF_INET;
+	client_addr.sin_port = htons(8080);
+	client_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+	if (connect(client_connect, reinterpret_cast<sockaddr*>(&client_addr), sizeof(client_addr)) == SOCKET_ERROR){
+		std::cerr << "Connect failed.\n";
+		closesocket(client_connect);
+		WSACleanup();
+		return 1;
+	}
+
+	int bytes = send(client_connect, client_message.c_str(), client_message.size(), 0);
+	if (bytes == SOCKET_ERROR){
+		std::cerr << "Send failed.\n";
+		closesocket(client_connect);
+		return 1;
+	} else {
+		std::cout << "Sent: " << bytes << " bytes";
+	}
+
+	closesocket(client_connect);
+	WSACleanup();
+	return 0;
+
+}
 
 int main() {
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("socket");
-        return 1;
-    }
-
-    sockaddr_in server{};
-    server.sin_family = AF_INET;
-    server.sin_port = htons(8080);
-    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
-
-    if (connect(sock, (sockaddr*)&server, sizeof(server)) < 0) {
-        perror("connect");
-        close(sock);
-        return 1;
-    }
-
-    const char* message = "Message sent by dropper";
-    send(sock, message, strlen(message), 0);
-
-    close(sock);
-    return 0;
+	int result = send_message(message);
+	return result;
 }
